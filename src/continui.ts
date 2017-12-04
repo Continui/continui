40859b83a85984aa07853d39d849f5fafe4b09ef
Francisco Mercedes <franciscomerdot@gmail.com>
2017-12-04 17:27:15 -0400

    Removing key value map and creating specific types.

--- src/continui.ts
+++ src/continui.ts
@@ -2,34 +2,37 @@ import { Step } from "./step";
 
 import * as fs from 'fs'
 import * as path from 'path'
-import { StepOptionDefinerFactory } from "./stepOptionDefinerFactory";
-import { CliStepOptionParser } from "./cliStepOptionParser";
-import { KeyValueMap } from "./keyValueMap";
 
-let privateScope = new WeakMap<Continui, {
+import { CliStepOptionParser } from "./cliStepOptionParser";
+import { StepOption } from "./stepOption";
+import { StepOptionMap, IdentifiedStepOptionMaps } from "./types";
+
+let privateScope: WeakMap<Continui, {
     steps: Step<any>[]
-    stepOptionDefinerFactory: StepOptionDefinerFactory
+    defaultIdentifiedStepOptionMaps: IdentifiedStepOptionMaps
     cliStepOptionParser: CliStepOptionParser
-}>();
+}> = new WeakMap();
 
 export class Continui {
 
     constructor(stepList: Step<any>[],
-                stepOptionDefinerFactory: StepOptionDefinerFactory,
-                cliStepOptionParser: CliStepOptionParser) {
+        cliStepOptionParser: CliStepOptionParser) {
+
         privateScope.set(this, {
-            steps: stepList,
-            stepOptionDefinerFactory: stepOptionDefinerFactory,
+            steps: [],
+            defaultIdentifiedStepOptionMaps: {},
             cliStepOptionParser: cliStepOptionParser
         })
+
+        this.loadSteps(...stepList);
     }
 
     public loadSteps(...steps: Step<any>[]): void {
-
+        
         let scope = privateScope.get(this);
 
-        steps.forEach(step => {            
-            step.defineOptions(scope.stepOptionDefinerFactory.getOptionDefinerForStep(step))
+        steps.forEach(step => {
+
             scope.steps.push(step)
         })
     }
@@ -38,31 +41,91 @@ export class Continui {
         this.execute(privateScope.get(this).cliStepOptionParser.parse(cliArguments))
     }
 
-    public execute(identifiedStepOptionKeyValueMap:KeyValueMap<KeyValueMap<any>>): void {
+    public execute(identifiedStepOptionMaps: IdentifiedStepOptionMaps): void {
+        let scope = privateScope.get(this);
 
-        let  fileIdentifiedStepOptionKeyValueMap:KeyValueMap<KeyValueMap<any>> = this.getidentifiedStepOptionKeyValueMapFromRoot();
+        let mainIdentifier: string = 'main';
+        let fromFileIdentifiedStepOptionMap: IdentifiedStepOptionMaps = this.getidentifiedStepOptionKeyValueMapFromRoot()
+        let combinedIdentifiedStepOptionMaps = Object.assign(scope.defaultIdentifiedStepOptionMaps || {},
+                                                             fromFileIdentifiedStepOptionMap || {},
+                                                             identifiedStepOptionMaps || {})
+        let mainStepOptionMap = combinedIdentifiedStepOptionMaps[mainIdentifier];
 
+        if (!mainStepOptionMap) {
+            throw new Error('Main step is missing, it looks that you are not using continui from CLI, so you must provide the main step parameters.');
+        }
+        
+        let toRunSteps: string[] = mainStepOptionMap.steps || [];
 
-        privateScope.get(this).steps.forEach(step => {
-            let stepOpionsMap: KeyValueMap<any> = Object.assign({},
-                                                             fileIdentifiedStepOptionKeyValueMap[step.identifier],                                                           
-                                                             identifiedStepOptionKeyValueMap[step.identifier])
+        if (mainStepOptionMap.needsVersion) {
+            this.displayVersion();
+            return;
+        }   
+
+        if (mainStepOptionMap.needsHelp) {
+            this.displayHelp();
+            return;
+        }
+
+        this.validateStepIdentifiers(toRunSteps)
+
+        toRunSteps.forEach(stepIdentifier => {
+
+            let stepOpionsMap: StepOptionMap = combinedIdentifiedStepOptionMaps[stepIdentifier];
+
+            // I assume that the find function will always retrieve a step because his existence is
+            // previously validated by the validateStepIdentifiers function.
+            let step: Step<any> = scope.steps.find(step => step.identifier == stepIdentifier);
 
             let context: any = step.createsNewContextFromOptionsMap(stepOpionsMap)
 
             step.createsRestaurationPoint(context)
 
-            try
-            {
+            try {
                 step.execute(context)
-            } catch(error) {
+            } catch (error) {
                 step.restore(context)
             }
         });
     }
 
-    private getidentifiedStepOptionKeyValueMapFromRoot(): KeyValueMap<KeyValueMap<any>> {
+    private getidentifiedStepOptionKeyValueMapFromRoot(): IdentifiedStepOptionMaps {
         let filePath: string = path.resolve(__dirname, 'continui.json')
         return fs.existsSync(filePath) ? require(filePath) : {};
+    }
+
+    private displayVersion() {
+        
+    }
+
+    private displayHelp() {
+
+    }
+
+    private hasStepWithIdentifier(stepIdentifier: string) {
+
+    }
+
+    private validateStepIdentifiers(stepIdentifiers: string[]) {
+        if (!stepIdentifiers.length) {
+            throw new Error('Must provided at least one step to run. eg. [continui mystep1 --mystep1.param1 "param1value" mystep2]')
+        }  
+
+        let stepErrorsMaps: {
+            stepIdentifier:string,
+            errors:string[]
+        }[] = [] 
+    
+
+        stepIdentifiers.forEach(stepIdentifier => {
+
+            let stepErrors: string[] = [];
+
+            if (!this.hasStepWithIdentifier(stepIdentifier)) {
+                stepErrors.push(`There is not any step with the identifier ${stepIdentifier}`)
+            }
+
+            
+        })
     }
 }
