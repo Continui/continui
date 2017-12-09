fafbb2096e94ba7b9fa59280354bb77d4703886b
Francisco Mercedes <franciscomerdot@gmail.com>
2017-12-09 17:18:05 -0400

    Missing documentation on step abstration.

--- src/step.ts
+++ src/step.ts
@@ -28,25 +28,28 @@ export interface Step<Context> {
 
     /**
      * Creates a restoration point based on the step to rollback the changes in case that the pipe flow breaks.
+     * @param stepOptionsValueMap Represents the options values provided to run the step.
      * @param context Represents the step execution context.
      */
     createsRestaurationPoint(stepOptionValueMap: StepOptionValueMap, context: Context): void | Promise<void> | IterableIterator<any>
 
     /**
      * Execute the step base on the given options and context.
+     * @param stepOptionsValueMap Represents the options values provided to run the step.
      * @param context Represents the step execution context.
      */
     execute(stepOptionValueMap: StepOptionValueMap, context: Context): void | Promise<void> | IterableIterator<any>;
     
     /**
      * Restore the step base on the given options and context.
+     * @param stepOptionsValueMap Represents the options values provided to run the step.
      * @param context Represents the step execution context.
      */
     restore(stepOptionValueMap: StepOptionValueMap, context: Context): void | Promise<void> | IterableIterator<any>;    
 
     /**
      * Creates and return an new context bases on the provided options.
-     * @param stepOptionKeyValueMap Represents the options map provided to run the step.
+     * @param stepOptionsValueMap Represents the options values provided to run the step.
      * @returns A new execution context bases on the provided options.
      */
     createsNewContextFromOptionsMap(stepOptionKeyValueMap: StepOptionValueMap): Context 
