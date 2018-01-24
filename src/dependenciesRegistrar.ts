93aa34297e565a20e509870526a76640d652d828
Francisco <franciscomerdot@gmail.com>
2018-01-23 22:18:13 -0400

    Registering dependencies into ativator.

--- src/dependenciesRegistrar.ts
+++ src/dependenciesRegistrar.ts
@@ -1,3 +1,4 @@
+import { Activator } from './domain/activator';
 import { BuildInLoggingService } from './build-in/services/buildInLoggingService';
 import { BuildInTextTemplateService } from './build-in/services/buildInTextTemplateService';
 import { BuildInTextSecureService } from './build-in/services/buildInTextSecureService';
@@ -6,39 +7,69 @@ import {
   StepActivationReferenceType,
   StepActivationReferenceMode,
 } from 'continui-step';
-import { BuildInTextTemplateContextProvider } from './build-in/services/buildInTextTemplateContextProvider';
+import {
+  BuildInTextTemplateContextProvider,
+} from './build-in/services/buildInTextTemplateContextProvider';
 import { BuildInStepsProvider } from './build-in/providers/buildInStepsProvider';
-import { Activator } from './index';
+import {
+  BuildInExecutionConfigurationMergingService,
+} from './build-in/services/buildInExecutionConfigurationMergingService';
+import {
+  BuildInFromFileExecutionConfigurationProvider,
+} from './build-in/providers/buildInFromFileExecutionConfigurationProvider';
+import {
+  BuildInCliExecutionConfigurationParsingService,
+} from './build-in/cli/buildIncliExecutionConfigurationParsingService';
 
-
-const buildInStepReferences: StepActivationReference[] = [{
-  alias: 'loggingService', 
-  target:  BuildInLoggingService,
-}, {
-  alias: 'textTemplateService',
-  target:  BuildInTextTemplateService,
-},{
-  alias: 'textTemplateContextProvider',
-  target: BuildInTextTemplateContextProvider,
-  mode: StepActivationReferenceMode.singelton,
-},{
-  alias: 'stepsProvider',
-  target: BuildInStepsProvider,
-},{
-  alias: 'textSecureService',
-  target:  BuildInTextSecureService,
-  mode: StepActivationReferenceMode.singelton,
-}];
 
 export class BuildInDependenciesRegistrar {
-  public registerBuilInReferencesIntoActivator(activator: Activator) {
-    
+  public registerBuilInReferencesIntoActivator(activator: Activator) {    
     activator.registerReference({
       alias: 'activator',       
       target:  activator,
       type: StepActivationReferenceType.constant,
     });
 
-    buildInStepReferences.forEach(reference => activator.registerReference(reference));
+    this.registerServicesIntoActivator(activator);
+    this.registerProvidersIntoActivator(activator);
+    this.registerCliDependenciesIntoActivator(activator);
+  }  
+
+  private registerServicesIntoActivator(activator: Activator) {
+    activator.registerReference({
+      alias: 'executionConfigurationMergingService',
+      target: BuildInExecutionConfigurationMergingService,
+    }).registerReference({
+      alias: 'loggingService', 
+      target:  BuildInLoggingService,
+    }).registerReference({
+      alias: 'textSecureService',
+      target:  BuildInTextSecureService,
+      mode: StepActivationReferenceMode.singelton,
+    }).registerReference({
+      alias: 'textTemplateContextProvider',
+      target: BuildInTextTemplateContextProvider,
+      mode: StepActivationReferenceMode.singelton,
+    }).registerReference({
+      alias: 'textTemplateService',
+      target:  BuildInTextTemplateService,
+    });
+  }
+
+  private registerProvidersIntoActivator(activator: Activator) {
+    activator.registerReference({
+      alias: 'fromFileExecutionConfigurationProvider',
+      target: BuildInFromFileExecutionConfigurationProvider,
+    }).registerReference({
+      alias: 'stepsProvider',
+      target: BuildInStepsProvider,
+    });
+  }
+
+  private registerCliDependenciesIntoActivator(activator: Activator) {
+    activator.registerReference({
+      alias: 'cliExecutionConfigurationParsingService',
+      target: BuildInCliExecutionConfigurationParsingService,
+    });
   }
 }
